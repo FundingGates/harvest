1b0033e1706da95db205185bda2a1904697942f2
Greggory Rothmeier <gregg@highgroove.com>
2012-10-30 17:11:20 -0400

    revert back to an OpenStruct for HarvestData

--- spec/harvest/harvest_data_spec.rb
+++ spec/harvest/harvest_data_spec.rb
@@ -15,4 +15,16 @@ describe Harvest::HarvestData do
       foo
     end
   end
+
+  it 'converts a hash to methods' do
+    foo =  Harvest::Foo.new(foo: "bar", bar: "baz", car: "fast")
+    foo.foo.should == "bar"
+    foo.bar.should == "baz"
+    foo.car.should == "fast"
+  end
+
+  it 'with less arguments' do
+    foo = Harvest::Foo.new(car: "faster")
+    foo.car.should == "faster"
+  end
 end
