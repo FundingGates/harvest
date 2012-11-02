c4336bec4c36de25afcff855258c3d2870bfbc20
Greggory Rothmeier <gregg@highgroove.com>
2012-11-02 11:35:26 -0400

    removed explicit VCR blocks

--- spec/harvest/client_spec.rb
+++ spec/harvest/client_spec.rb
@@ -14,110 +14,84 @@ describe Harvest::Client do
 
   describe '#who_am_i?' do
     it 'retrieves the current user' do
-      VCR.use_cassette('who_am_i') do
-        me = subject.who_am_i?
-        me.email.should == 'accountingpackages@fundinggates.com'
-      end
+      me = subject.who_am_i?
+      me.email.should == 'accountingpackages@fundinggates.com'
     end
 
     it 'raises an exception if Harvest returns an error' do
-      VCR.use_cassette('bad_token') do
-        client = Harvest::Client.new("bad-token")
-        expect { client.who_am_i? }.to raise_error(Harvest::AuthorizationFailure, /token provided is expired/)
-      end
+      client = Harvest::Client.new("bad-token")
+      expect { client.who_am_i? }.to raise_error(Harvest::AuthorizationFailure, /token provided is expired/)
     end
   end
 
   describe '#people' do
     it 'retrieves the users organization' do
-      VCR.use_cassette('people') do
-        people = subject.people
-        people[0].email.should == 'accountingpackages@fundinggates.com'
-      end
+      people = subject.people
+      people[0].email.should == 'accountingpackages@fundinggates.com'
     end
   end
 
   describe '#company' do
     it 'retrieves information about the company' do
-      VCR.use_cassette('company') do
-        company = subject.company
-        company.name.should == 'Funding Gates'
-      end
+      company = subject.company
+      company.name.should == 'Funding Gates'
     end
   end
 
   describe '#invoices' do
     it 'returns an array of invoices' do
-      VCR.use_cassette('invoices') do
-        invoices = subject.invoices
-        invoices[0].should be_a Harvest::Invoice
-      end
+      invoices = subject.invoices
+      invoices[0].should be_a Harvest::Invoice
     end
 
     it 'accepts query params' do
-      VCR.use_cassette('invoice_query_params') do
-        cutoff = Time.utc(2012, 10, 1)
-        invoices = subject.invoices(updated_since: cutoff)
-        expect { invoices.delete_if { |inv| inv.updated_at < cutoff } }.to_not change(invoices, :length)
-      end
+      cutoff = Time.utc(2012, 10, 1)
+      invoices = subject.invoices(updated_since: cutoff)
+      expect { invoices.delete_if { |inv| inv.updated_at < cutoff } }.to_not change(invoices, :length)
     end
   end
 
   describe '#invoice' do
     it 'returns an invoice from id' do
-      VCR.use_cassette('invoice') do
-        invoice = subject.invoice('1882548')
-        invoice.amount.should == 435.5
-        invoice.updated_at.should be_a_kind_of Time
-      end
+      invoice = subject.invoice('1882548')
+      invoice.amount.should == 435.5
+      invoice.updated_at.should be_a_kind_of Time
     end
 
     it 'raises an exception if an invoice id is not valid' do
-      VCR.use_cassette('invoice_invalid') do
-        expect { subject.invoice('abc123') }.to raise_error Harvest::InvoiceNotFound
-      end
+      expect { subject.invoice('abc123') }.to raise_error Harvest::InvoiceNotFound
     end
   end
 
   describe '#customers' do
     it 'list customers' do
-      VCR.use_cassette('customers') do
-        customers = subject.customers
-        customers[0].name.should == "A Cavallo Violins, LLC"
-      end
+      customers = subject.customers
+      customers[0].name.should == "A Cavallo Violins, LLC"
     end
 
     it 'accepts query params' do
-      VCR.use_cassette('customer_query_params') do
-        cutoff = Time.utc(2012, 10, 1)
-        customers = subject.customers(updated_since: cutoff)
-        expect { customers.delete_if { |inv| inv.updated_at < cutoff } }.to_not change(customers, :length)
-      end
+      cutoff = Time.utc(2012, 10, 1)
+      customers = subject.customers(updated_since: cutoff)
+      expect { customers.delete_if { |inv| inv.updated_at < cutoff } }.to_not change(customers, :length)
     end
   end
 
   describe '#customer' do
     it 'retrieves a customer' do
-      VCR.use_cassette('customer') do
-        customer = subject.customer('1268610')
-        customer.name.should == "Whitworth Construction"
-      end
+      customer = subject.customer('1268610')
+      customer.name.should == "Whitworth Construction"
     end
   end
 
   describe '#contacts_for_customer' do
     it 'retrieves a contact for a customer' do
-      VCR.use_cassette('contacts_for_customer') do
-        contacts = subject.contacts_for_customer('1268614')
-        contacts.first.first_name.should == "Jordon"
-      end
+      contacts = subject.contacts_for_customer('1268614')
+      contacts.first.first_name.should == "Jordon"
     end
 
     it 'without contacts' do
-      VCR.use_cassette('customer_with_no_contacts') do
-        contacts = subject.contacts_for_customer('1268610')
-        contacts.should == []
-      end
+      contacts = subject.contacts_for_customer('1268610')
+      contacts.should == []
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,16 @@ VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock # or :fakeweb
 end
+
+RSpec.configure do |config|
+  # Add VCR to all tests
+  config.around(:each) do |example|
+    options = example.metadata[:vcr] || {}
+    if options[:record] == :skip 
+      VCR.turned_off(&example)
+    else
+      name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
+      VCR.use_cassette(name, options, &example)
+    end
+  end
+end 
